1344552de4b83c918c6b59dbd14ec291f93d918f
ㅤㅤ ㅤㅤwho4004 <rhiad.ciccoli@gmail.com>
2021-04-13 02:09:24 -0300

    added auth route, auth middleware, use contacts post

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,77 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+const auth = require('../middleware/auth')
+const User = require('../models/User');
 
 /*  @route  GET api/auth
     @desc   get logged in user
     @access private          */
 
-router.get('/', (req, res) => {
-    res.send('get logged in user')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("server error")        
+    }
 })
 
 /*  @route  POST api/auth
     @desc   Auth user & get token
     @access public          */
 
-    router.post('/', (req, res) => {
-        res.send('log in user')
-    })
+router.post('/', [
+    check('email', 'please enter a valid email').isEmail(),
+    check('password', 'password is required').exists()
+    ],
+    async (req, res) => {
+    //handling da validação de erros
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    const {email, password} = req.body
+
+    try {
+        //verificando se o email esta cadastrado, se não, retorna status com msg
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({msg: 'Invalid credentials'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({msg: 'Invalid credentials'})
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {
+                expiresIn: 360000
+            },
+            (err, token) =>{
+                if (err) throw err;
+                res.json({ token })
+            }
+        )
+    } catch (error) {
+        console.error(e.message)
+        res.status(500).send('server error')
+    }
+    
+})
 
 
 //exporting
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth')
+const { check, validationResult} = require('express-validator')
+const User = require('../models/User')
+const Contact = require('../models/Contact')
 
 /*  @route  GET api/contacts
     @desc   Get all users contacts
     @access Private          */
 
-router.get('/', (req, res) => {
-    res.send('Get all contacts')
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({user: req.user.id}).sort({date: -1})
+        res.json(contacts)
+    } catch (error) {
+        console.error(error.message)
+        res,status(500).send('server error')
+    }
 })
 
 
@@ -15,8 +25,33 @@ router.get('/', (req, res) => {
     @desc   Add new contact
     @access Private          */
 
-    router.post('/', (req, res) => {
-        res.send('add contact')
+    router.post('/', [auth, [
+        check('name', 'name is required').not().isEmpty(),
+        check('email', 'please, enter a valid email for the contact').isEmail()
+
+    ]], 
+    async (req, res) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array() })
+        }
+
+        const {name, email, phone, type} = req.body 
+
+        try {
+            const newContact = Contact({
+                name,
+                email,
+                phone,
+                type,
+                user: req.user.id
+            })
+            const contact = await newContact.save()
+            res.json(contact)
+        } catch (e) {
+            console.error(e.message)
+            res.status(500).send('Server error')
+        }
     })
 
 
